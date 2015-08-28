f76553a791bf1b6af80028a998bebea0841debd7
Tibor Kertesz <hummerdikk@gmail.com>
2015-08-28 09:51:01 +0200

    More work on the ViewController

--- MQTTLibTest/ViewController.m
+++ MQTTLibTest/ViewController.m
@@ -22,6 +22,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //If we are starting the mqtt first, let's do some setup
     if (!self.mqttManager) {
         self.mqttManager = [[MQTTSessionManager alloc] init];
         self.mqttManager.delegate = self;
@@ -45,12 +47,70 @@
                             willQos:MQTTQosLevelExactlyOnce
                      willRetainFlag:FALSE
                        withClientId:[UIDevice currentDevice].name];
+    }else{
+        //else we can reconnect to the last mqtt server
+        [self.mqttManager connectToLast];
     }
+    
+    [self.mqttManager addObserver:self
+                       forKeyPath:@"state"
+                          options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew
+                          context:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    switch (self.mqttManager.state) {
+        case MQTTSessionManagerStateClosed:
+            self.MessageLabel.text = @"closed";
+            self.DisconnectButton.enabled = false;
+            self.ConnectButton.enabled = false;
+            break;
+        case MQTTSessionManagerStateClosing:
+            self.MessageLabel.text = @"closing";
+            self.DisconnectButton.enabled = false;
+            self.ConnectButton.enabled = false;
+            break;
+        case MQTTSessionManagerStateConnected:
+            self.MessageLabel.text = [NSString stringWithFormat:@"connected as %@-%@",
+                                [UIDevice currentDevice].name,
+                                self.tabBarItem.title];
+            self.DisconnectButton.enabled = true;
+            self.ConnectButton.enabled = false;
+            [self.mqttManager sendData:[@"joins" dataUsingEncoding:NSUTF8StringEncoding]
+                             topic:[NSString stringWithFormat:@"%@/%@-%@",
+                                    self.base,
+                                    [UIDevice currentDevice].name,
+                                    self.tabBarItem.title]
+                               qos:MQTTQosLevelExactlyOnce
+                            retain:FALSE];
+            
+            break;
+        case MQTTSessionManagerStateConnecting:
+            self.MessageLabel.text = @"connecting";
+            self.DisconnectButton.enabled = false;
+            self.ConnectButton.enabled = false;
+            break;
+        case MQTTSessionManagerStateError:
+            self.MessageLabel.text = @"error";
+            self.DisconnectButton.enabled = false;
+            self.ConnectButton.enabled = false;
+            break;
+        case MQTTSessionManagerStateStarting:
+        default:
+            self.MessageLabel.text = @"not connected";
+            self.DisconnectButton.enabled = false;
+            self.ConnectButton.enabled = true;
+            break;
+    }
+}
+
+- (IBAction)conn :(id)sender)
 
 @end
